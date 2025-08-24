251f0791b093001707710fb8f53020d7be24a42b
Kareem Khater <118448251+AnaaaKareem@users.noreply.github.com>
2025-08-24 10:21:30 +0000

    V19

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,32 +1,52 @@
+resource "aws_security_group" "eks_cluster" {
+name = "karim-eks-cluster-sg"
+description = "Cluster communication"
+vpc_id = var.vpc_id
+
+
+  ingress {
+    description = "Nodes to control plane"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "karim-eks-cluster-sg"
+  }
+}
+
 resource "aws_eks_cluster" "task-3-v2" {
-  name = "Task-Three-v2"
-  role_arn = aws_iam_role.cluster.arn
-  version = "1.33"
+  name     = "Task-Three-v2"
+  role_arn = aws_iam_role.eks-cluster.arn
+  version  = "1.33"
 
   access_config {
     authentication_mode = "API"
   }
 
-  upgrade_policy {
-    support_type = "STANDARD"
-  }
-
   vpc_config {
-    subnet_ids = var.private_subnets
-    
-    # security_group_ids = 
-    endpoint_public_access = true
+    subnet_ids              = var.private_subnets
+    security_group_ids      = [aws_security_group.eks_cluster.id]
+    endpoint_public_access  = true
     endpoint_private_access = true
-  }
-
-  zonal_shift_config {
-    enabled = false
   }
 
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
   ]
 }
+
 
 resource "aws_eks_addon" "addons" {
   for_each = toset(var.addons)
@@ -36,40 +56,31 @@ resource "aws_eks_addon" "addons" {
 }
 
 resource "aws_eks_node_group" "eks-node" {
-  cluster_name = aws_eks_cluster.task-3-v2.name
+  cluster_name    = aws_eks_cluster.task-3-v2.name
   node_group_name = "node"
-  node_role_arn = aws_iam_role.node.arn
-  subnet_ids = var.private_subnets
-  instance_types = [ "t3.small" ]
+  node_role_arn   = aws_iam_role.eks-node.arn
+  subnet_ids      = var.private_subnets
+  instance_types  = ["t3.small"]
 
   scaling_config {
     desired_size = 2
-    min_size = 2
-    max_size = 3
+    min_size     = 2
+    max_size     = 3
   }
 
   update_config {
     max_unavailable = 1
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
+  ]
 }
 
-resource "aws_iam_role" "node" {
-  name = "eks-node-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
 
-resource "aws_iam_role" "cluster" {
+resource "aws_iam_role" "eks-cluster" {
   name = "eks-cluster-karim"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -90,7 +101,7 @@ resource "aws_iam_role" "cluster" {
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.cluster.name
+  role       = aws_iam_role.eks-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
